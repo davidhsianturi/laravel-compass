9d3e213bbbdc4ae16ea8b2d015b8dea323f87cf0
David H. Sianturi <davidhasiholans@gmail.com>
2019-10-05 10:38:14 +0700

    recompile

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -10,9 +10,7 @@ export default {
 
 <template>
     <div class="px-6 py-4 bg-secondary">
-        <p class="text-gray-700 text-xl">REST assistant for building APIs in Laravel.</p>
-        <p class="text-gray-600">
-            <a href="#" target="_blank" class="hover:text-gray-700">Documentation</a>
-        </p>
+        <p class="text-gray-700 text-xl">Laravel REST client</p>
+        <p class="text-gray-700 text-xl">for testing and build API documentation.</p>
     </div>
 </template>
