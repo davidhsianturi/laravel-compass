8b2a0991ff4733cb67eb537b92104f02055e363e
akbarcandra <akbarcandrawarsa@gmail.com>
2019-11-05 17:09:18 +0700

    style: remove semicolon & use string literal

--- resources/js/components/HttpResponseSize.vue
+++ resources/js/components/HttpResponseSize.vue
@@ -4,11 +4,11 @@ export default {
 
     computed: {
         size () {
-            const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
-            const bytes = this.response.headers['content-length'] || 0;
-            if (bytes == 0) return '0 B';
-            var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-            return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+            const sizes = ['B', 'KB', 'MB', 'GB', 'TB']
+            const bytes = this.response.headers['content-length'] || 0
+            if (bytes == 0) return '0 B'
+            var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+            return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`
         }
     }
 }
