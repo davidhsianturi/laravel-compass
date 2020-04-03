c226d003e1ce078c9cac0ecba1303c773c5cc5b9
David H. Sianturi <davidhasiholans@gmail.com>
2020-04-03 13:06:01 +0700

    recompile.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('/vendor/compass/favicon.ico') }}">
+
     <title>Compass - {{ config('app.name') }}</title>
 
     <link href="{{ mix('app.css', 'vendor/compass') }}" rel="stylesheet" type="text/css">
