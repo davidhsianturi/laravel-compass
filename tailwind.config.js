dc3bbc64be9241993eecaac3ae68b216d7c2ed66
David H. Sianturi <davidhasiholans@gmail.com>
2020-09-15 05:55:22 +0700

    Update TailwindCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+    purge: {
+        content: [
+            './resources/**/*.blade.php',
+            './resources/**/*.vue',
+            './resources/**/*.js'
+        ],
+        options: {
+            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/]
+        }
+    },
     theme: {
         boxShadow: {
             default: '0 15px 35px 0 rgba(94, 59, 59, .1)'
@@ -17,7 +27,7 @@ module.exports = {
             },
             spacing: {
                 '72': '18rem',
-                '80': '20rem',
+                '80': '20rem'
             },
             padding: {
                 '5/6': '83.3333333%'
@@ -28,5 +38,9 @@ module.exports = {
         tableLayout: ['responsive', 'hover', 'focus'],
         borderColor: ['responsive', 'hover', 'focus', 'focus-within']
     },
-    plugins: []
+    plugins: [],
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true
+    }
 }
